f73ba35ce2e56a3d5d1c7ec74b4bef9f7651db04
Super Probably Level <122291337+SPLCoding@users.noreply.github.com>
2023-01-18 19:15:35 -0300

    Update FlxRuntimeShader.hx

--- source/flixel/addons/display/FlxRuntimeShader.hx
+++ source/flixel/addons/display/FlxRuntimeShader.hx
@@ -276,76 +276,120 @@ class FlxRuntimeShader extends FlxShader
 			__paramBool = new Array();
 			__paramFloat = new Array();
 			__paramInt = new Array();
-
 			__processGLData(glVertexSource, "attribute");
 			__processGLData(glVertexSource, "uniform");
 			__processGLData(glFragmentSource, "uniform");
 		}
 
-		@:privateAccess
 		if (__context != null && program == null)
 		{
+			@:privateAccess
 			var gl = __context.gl;
 
-			var precisionHeaders = buildPrecisionHeaders();
-			var versionHeader = '#version ${_glslVersion}\n';
+			var prefix = '';
 
-			var vertex = StringTools.replace(glVertexSource, PRAGMA_PRECISION, precisionHeaders);
-			vertex = StringTools.replace(vertex, PRAGMA_VERSION, versionHeader);
-			var fragment = StringTools.replace(glFragmentSource, PRAGMA_PRECISION, precisionHeaders);
-			fragment = StringTools.replace(fragment, PRAGMA_VERSION, versionHeader);
-			
+			#if (js && html5)
+			prefix += (precisionHint == FULL ? "precision mediump float;\n" : "precision lowp float;\n");
+			#else
+			prefix += "#ifdef GL_ES\n"
+				+ (precisionHint == FULL ? "#ifdef GL_FRAGMENT_PRECISION_HIGH\n"
+					+ "precision highp float;\n"
+					+ "#else\n"
+					+ "precision mediump float;\n"
+					+ "#endif\n" : "precision lowp float;\n")
+				+ "#endif\n\n";
+			#end
+
+			var vertex = prefix + glVertexSource;
+			var fragment = prefix + glFragmentSource;
+
 			var id = vertex + fragment;
 
-			if (__context.__programs.exists(id)) {
-				// Use the existing program if it has been compiled.
+			@:privateAccess
+			if (__context.__programs.exists(id))
+			{
+				@:privateAccess
 				program = __context.__programs.get(id);
-			} else {
-				// Build the program.
+			}
+			else
+			{
 				program = __context.createProgram(GLSL);
+
+				@:privateAccess
 				program.__glProgram = __createGLProgram(vertex, fragment);
+
+				@:privateAccess
 				__context.__programs.set(id, program);
 			}
 
-			if (program != null) {
+			if (program != null)
+			{
+				@:privateAccess
 				glProgram = program.__glProgram;
 
-				// Map attributes for each type.
-
-				for (input in __inputBitmapData) {
-					if (input.__isUniform) {
+				for (input in __inputBitmapData)
+				{
+					@:privateAccess
+					if (input.__isUniform)
+					{
+						@:privateAccess
 						input.index = gl.getUniformLocation(glProgram, input.name);
-					} else {
+					}
+					else
+					{
+						@:privateAccess
 						input.index = gl.getAttribLocation(glProgram, input.name);
 					}
 				}
 
-				for (parameter in __paramBool) {
-					if (parameter.__isUniform) {
+				for (parameter in __paramBool)
+				{
+					@:privateAccess
+					if (parameter.__isUniform)
+					{
+						@:privateAccess
 						parameter.index = gl.getUniformLocation(glProgram, parameter.name);
-					} else {
+					}
+					else
+					{
+						@:privateAccess
 						parameter.index = gl.getAttribLocation(glProgram, parameter.name);
 					}
 				}
 
-				for (parameter in __paramFloat) {
-					if (parameter.__isUniform) {
+				for (parameter in __paramFloat)
+				{
+					@:privateAccess
+					if (parameter.__isUniform)
+					{
+						@:privateAccess
 						parameter.index = gl.getUniformLocation(glProgram, parameter.name);
-					} else {
+					}
+					else
+					{
+						@:privateAccess
 						parameter.index = gl.getAttribLocation(glProgram, parameter.name);
 					}
 				}
 
-				for (parameter in __paramInt) {
-					if (parameter.__isUniform) {
+				for (parameter in __paramInt)
+				{
+					@:privateAccess
+					if (parameter.__isUniform)
+					{
+						@:privateAccess
 						parameter.index = gl.getUniformLocation(glProgram, parameter.name);
-					} else {
+					}
+					else
+					{
+						@:privateAccess
 						parameter.index = gl.getAttribLocation(glProgram, parameter.name);
 					}
 				}
 			}
 		}
 	}
+
 
 	private var __fieldList:Array<String> = null;
 	private function thisHasField(name:String) {
